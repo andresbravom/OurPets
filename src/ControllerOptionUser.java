2f577eb43eca7e9f04c28af00efa5c55e252441a
Andrés Bravo <andresbravomontes89@gmail.com>
2019-02-16 13:38:26 +0100

    writeFileFuntion

--- src/ControllerOptionUser.java
+++ src/ControllerOptionUser.java
@@ -51,3 +51,16 @@ public class ControllerOptionUser {
     }
 
 }
+
+/*
+public void writeNewUser() {
+try (BufferedWriter bw = new BufferedWriter(new FileWriter("users.txt", true))) {
+    bw.write(tfUsername.getText());
+    bw.newLine();
+    bw.write(tfPassword.getText());
+    bw.newLine();
+} catch (IOException e) {
+    e.printStackTrace();
+
+}
+}*/
