7729e29f0e8d00306ace5f9a002906d6ac755c55
Andrés Bravo <andresbravomontes89@gmail.com>
2019-02-19 15:02:38 +0100

    clear code

--- src/Main.java
+++ src/Main.java
@@ -12,29 +12,6 @@ public class Main {
 		Admins admin3 = new Admins ("Admin3", "qwerty");
 	
 		General.Inicio();
-		/*
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Introducir usuario: ");
-		String usuario = sc.nextLine();
-		System.out.println("Introducir contraseña: ");
-		String contraseña = sc.nextLine();
-		
-		if(Admins.authentication(usuario,contraseña)) {
-			if(Admins.actualAdmin.equals(adminM)) {	
-			l.mostrarListaGeneral();
-			l.mostrarListados();
-			
-			}else if(Admins.actualAdmin.equals(admin1)) {
-				l.mostrarListados();
-			}else if(Admins.actualAdmin.equals(admin2)) {
-				l.mostrarListados();
-			}else if(Admins.actualAdmin.equals(admin3)) {
-				l.mostrarListados();
-			}
-		}			
-	}
-		l.addUser();	*/
-
 
 	}
 }
